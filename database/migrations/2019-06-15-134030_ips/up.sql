f16eaa46926767043afd9d74a9c88fa8874bbb4d
Jeremy Bobbin <jer@jer.cx>
2020-07-19 11:34:58 -0700

    remove rust & most of the frontend

--- database/migrations/2019-06-15-134030_ips/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE IF NOT EXISTS hashed_ips (
-    id SERIAL PRIMARY KEY,
-    hashed_ip VARCHAR NOT NULL UNIQUE
-);
-
--- database/src/schema.rs
+++
@@ -1,6 +0,0 @@
-table! {
-    hashed_ips (id) {
-        id -> Int4,
-        hashed_ip -> Varchar,
-    }
-}
