01e253da1a38b74c5fe63e05391ff8da373b3f12
Jeremy Bobbin <jer@jer.cx>
2020-07-22 07:20:12 -0700

    only run when the document is loaded.

--- js/stream.js
+++ js/stream.js
@@ -1,3 +1,20 @@
+function attachOnLoad(fn) {
+	if(window.attachEvent) {
+		window.attachEvent('onload', fn);
+	} else {
+		if(window.onload) {
+			var curronload = window.onload;
+			var newonload = function(evt) {
+				curronload(evt);
+				fn(evt);
+			};
+			window.onload = newonload;
+		} else {
+			window.onload = fn;
+		}
+	}
+}
+
 function handleMediaError(event, data) {
 	if (data.fatal) {
 		switch(data.type) {
@@ -18,35 +35,37 @@ function handleMediaError(event, data) {
 	}
 }
 
-var video = document.getElementById('stream');
-var videoSrc = '/public/stream/streaming.m3u8';
-if (Hls.isSupported()) {
-	console.log("works");
-	var hls = new Hls();
-	hls.loadSource(videoSrc);
-	hls.attachMedia(video);
-	hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-		console.log("video and hls.js are now bound together!");
+attachOnLoad(function() {
+	var video = document.getElementById('video');
+	var videoSrc = '/public/stream/streaming.m3u8';
+	if (Hls.isSupported()) {
+		console.log("works");
+		var hls = new Hls();
 		hls.loadSource(videoSrc);
-		hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-			console.log("manifest loaded, found " + data.levels.length + " quality level");
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+			console.log("video and hls.js are now bound together!");
+			hls.loadSource(videoSrc);
+			hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+				console.log("manifest loaded, found " + data.levels.length + " quality level");
+			});
 		});
-	});
-	video.onclick = function() {
-		video.play();
-	}
-	video.addEventListener('loadedmetadata', function() {
 		video.onclick = function() {
 			video.play();
 		}
-	});
-	video.onended = function() {
-		console.log("video ended");
-		hls.loadSource(videoSrc);
-		hls.attachMedia(video);
+		video.addEventListener('loadedmetadata', function() {
+			video.onclick = function() {
+				video.play();
+			}
+		});
+		video.onended = function() {
+			console.log("video ended");
+			hls.loadSource(videoSrc);
+			hls.attachMedia(video);
+		}
+		hls.liveDurationInfinity = true;
+		hls.on(Hls.Events.ERROR, handleMediaError);
+	} else {
+		console.log("doesn't work.");
 	}
-	hls.liveDurationInfinity = true;
-	hls.on(Hls.Events.ERROR, handleMediaError);
-} else {
-	console.log("doesn't work.");
-}
+});
