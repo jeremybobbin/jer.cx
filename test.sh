4d11a3d85530209587335a8bd22286294c9cfdd2
Jeremy Bobbin <jer@jer.cx>
2020-07-23 06:59:31 -0700

    start stream & add webroot on test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+PATH=$PATH:build/usr/local/bin
+WEBROOT=build/srv/http
+export PATH WEBROOT
+
+trap cleanup 0 1 2 3 6
+
+cleanup() {
+	echo "caught some signal. exitting" 2>/dev/null
+	kill $quark
+}
+
 make install DESTDIR=build
-export PATH=$PATH:build/usr/local/bin
-exec quark -p 8080 -h 0.0.0.0 -d build/srv/http -x
+quark -p 8080 -h 0.0.0.0 -d build/srv/http -x &
+quark=$!
+
+stream.sh
