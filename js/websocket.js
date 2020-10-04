01768a56b29925a16b6ea25cc1458e33e4bb7514
Jeremy Bobbin <jer@jer.cx>
2020-10-04 12:20:01 -0700

    websocket: listen on 8444 if https

--- js/websocket.js
+++ js/websocket.js
@@ -1,3 +1,5 @@
+var user, message, messages;
+
 function attachOnLoad(fn) {
 	if(window.attachEvent) {
 		window.attachEvent('onload', fn);
@@ -17,15 +19,37 @@ function attachOnLoad(fn) {
 
 attachOnLoad(function() {
 	// Create WebSocket connection.
-	const socket = new WebSocket('ws://jer.cx:8081');
+	const socket = new WebSocket(location.protocol === 'https:' ?
+		"wss://jer.cx:8444" :
+		"ws://jer.cx:8081");
 
 	// Connection opened
 	socket.addEventListener('open', function (event) {
-	    socket.send('Hello Server!');
+		var cookies, id;
+		cookies = document.cookie.split("; ");
+		for (i in cookies) {
+			var pair = cookies[i].split("=");
+			if (pair[0] && pair[0] == "ID") {
+				id = pair[1];
+				break;
+			}
+		}
+
+		if (!id) {
+			console.log("err: no ID in cookies; closing socket");
+			socket.close();
+		} else {
+			socket.send(id + ': 0');
+		}
 	});
 
 	// Listen for messages
 	socket.addEventListener('message', function (event) {
-	    console.log('Message from server ', event.data);
+		var s = event.data.split(':');
+		user = s.shift();
+		message = s.join(':');
+		console.log(user + " says " + event.data);
+
+		/* uimplemented */
 	});
 });
